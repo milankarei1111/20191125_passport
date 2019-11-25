ae391d34a00e8bb17fff0114ea1bd9c54685fc57
wanru_tsai <milankarei1111@gmail.com>
2019-11-25 14:08:54 +0800

    add wantsJson()、expectsJson()

--- app/Http/Requests/BaseRequest.php
+++ app/Http/Requests/BaseRequest.php
@@ -6,25 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class BaseRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    public function wantsJson()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function expectsJson()
     {
-        return [
-            //
-        ];
+        return true;
     }
 }
