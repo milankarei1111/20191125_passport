e75715c9ea0840ae0eb4ea96b8846c9ea229ab4f
wanru_tsai <milankarei1111@gmail.com>
2019-11-25 14:13:48 +0800

    加入註冊功能:驗證、新增、發送請求、建構子功能 路由依序開放功能(未填寫的功能先註解)

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -2,24 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 class PassportController extends Controller
 {
-    //
+    protected $clientId;
+    protected $clientSecret;
+
     public function __construct()
     {
-
+        $this->middleware('auth')->except('login', 'register', 'refresh');
+        // 使用緩存方式存取 clientId與 clientSecret
+        $client = Cache::remember('password_client', 60*5, function () {
+            return DB::table('oauth_clients')->where('id', 2)->first();
+        });
+        $this->clientId = $client->id;
+        $this->clientSecret = $client->secret;
     }
 
     protected function username()
     {
-      
+        return 'email';
     }
 
     public function register()
     {
-
+        $this->validator(request()->all())->validate();
+        $this->create(request()->all());
+        return $this->getToken();
     }
 
     public function login()
@@ -39,16 +54,35 @@ class PassportController extends Controller
 
     protected function validator(array $data)
     {
-
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
+        ]);
     }
 
     protected function create(array $data)
     {
-
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => password_hash($data['password'], PASSWORD_DEFAULT),
+        ]);
     }
 
     private function getToken()
     {
-
+        // 偽造一個http請求
+        $response = (new Client())->post('http://localhost:9989/api/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'password',
+                'username' => request('email'),
+                'password' => request('password'),
+                'client_id' => $this->clientId,
+                'client_secret' => $this->clientSecret,
+                'scope' => '*',
+            ]
+        ]);
+        return $response->getBody();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
 
 Route::post('/register', 'PassportController@register');
-Route::post('/login', 'PassportController@login');
-Route::post('/logout', 'PassportController@logout');
-Route::post('/refresh', 'PassportController@refresh');
+// Route::post('/login', 'PassportController@login');
+// Route::post('/logout', 'PassportController@logout');
+// Route::post('/refresh', 'PassportController@refresh');
 
 // 授權後才可訪問的測試頁面
 Route::get('/test', function(){
