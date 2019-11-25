77f2ddeacf19925119ae7b9d5856a124670d4a36
wanru_tsai <milankarei1111@gmail.com>
2019-11-25 11:09:32 +0800

    設定方法

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -7,4 +7,48 @@ use Illuminate\Http\Request;
 class PassportController extends Controller
 {
     //
+    public function __construct()
+    {
+
+    }
+
+    protected function username()
+    {
+      
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+
+    public function refresh()
+    {
+
+    }
+
+    protected function validator(array $data)
+    {
+
+    }
+
+    protected function create(array $data)
+    {
+
+    }
+
+    private function getToken()
+    {
+
+    }
 }
