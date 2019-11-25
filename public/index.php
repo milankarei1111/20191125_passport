9acf35a796f9628980e618b21b1db9eb741943fe
wanru_tsai <milankarei1111@gmail.com>
2019-11-25 11:55:59 +0800

    更正為 App\Http\Requests\BaseRequest::capture()

--- public/index.php
+++ public/index.php
@@ -54,7 +54,7 @@ $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
     // $request = Illuminate\Http\Request::capture()
-    $request = \App\Http\Requests\BaseRequest::class
+    $request = App\Http\Requests\BaseRequest::capture()
 );
 
 $response->send();
